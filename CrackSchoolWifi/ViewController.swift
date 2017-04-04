f69ab3e97646f582fcb415854768354fadef876e
Henri Jäger <henerii@henris-mbp>
2017-04-04 20:14:28 +0200

    Initial Commit

---
+++ CrackSchoolWifi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrackSchoolWifi
+//
+//  Created by Henri Jäger on 04.04.17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
